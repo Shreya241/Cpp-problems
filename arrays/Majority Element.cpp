a59a77af2c4f165bbe883f651820cf8385a12724
Shreya <shreyaagrawal.kunra@gmail.com>
2022-07-02 14:57:46 +0530

    2nd

--- arrays/Majority Element.cpp
+++ arrays/Majority Element.cpp
@@ -1,12 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int wave(int arr[],int n,int x){
-    
+int wave(int arr[], int n)
+{
+    sort(arr, arr + n);
+    int count = 1;
+    int i = 0;
+    int k;
+    while (i < n)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            count++;
+            k=i;
+            i++;
+        }
+        else
+        {
+            i++;
+            count = 1;
+        }
+        if (count > n / 2)
+        {
+            return arr[k];
+        }
+    }
+    return -1;
 }
 
-int main(){
-   int n=6;int x=54;
-   int a1[n] = {10, 22, 28, 29, 30, 40};
-   cout<<wave(a1,n,x);
-
+int main()
+{
+    int n = 3;
+    int a1[n] = {1,2,3};
+    cout << wave(a1, n);
 }
