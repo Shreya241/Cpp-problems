2acaa60a27466495fb06035d983ea4fe0154ec0e
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-02-16 11:28:45 +0530

    cheap travel

--- CF solutions/Cheap Travel.cpp
+++ CF solutions/Cheap Travel.cpp
@@ -4,29 +4,19 @@ using namespace std;
  
 int main(){
   int n,m,a,b;cin>>n>>m>>a>>b;
-  int c=0;
-  while(n>0){
-    if(a==1){
-      if(a*n>b/m){
-      cout<<b;}
-      else cout<<a*n;
-      return 0;
+  int s,c,k;
+  if(m>n && b<n*a){
+      c=b;
     }
-    else if(b/m<=a){
-      if(n%m==0){
-        n=n-m;
-        c+=b;
-      }else if(b>a){
-        n--;
-        c+=a;
-      }else{
-        n=n-m;
-        c+=b;
-      }
-    }else{
-      cout<<n*a;
-      return 0;
-    }
+  else if(b/m<a){
+    s=n/m;
+    c=s*b;
+    k=n%m;
+    c=min(c+k*a,c+b);
+  }else if(b/m==a){
+    c=n*a;
+  }else{
+    c=a*n;
   }
   cout<<c;
 }
