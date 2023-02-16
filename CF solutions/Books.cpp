d98ea91cc95f7062cc9c2833a4f86f92dae4275d
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-02-17 00:57:29 +0530

    books

--- CF solutions/Books.cpp
+++ CF solutions/Books.cpp
@@ -3,28 +3,28 @@
 using namespace std;
  
 int main(){
-   long long n,t,c=0,s=0;
+   long long n,t,i;
    cin>>n;
    cin>>t;
    long long arr[n];
    for(long long i=0; i<n; i++){
     cin>>arr[i];
    }
-   long long i=0,j=n-1;
-   if(n==1 && t<arr[0]){
-      cout<<0;
-      return 0;
-   }else if(n==1 && t>arr[0]){
-      cout<<1;
-      return 0;
-   }
-   while(c<t && i<n){
-      c+=arr[i];
-      if(c<t)i++;
-   }
-   while(s<t && j>=0){
-      s+=arr[j];
-      if(s<t)j--;
-   }
-   cout<<max(i,n-j+1);
+   long long j=-1,sum=0,ans=0;
+        for(i=0;i<n;i++)
+        {
+            if(sum+arr[i]<=t)
+                sum+=arr[i];
+            else
+            {
+                sum+=arr[i];
+                while(sum>t)
+                {
+                    j++;
+                    sum-=arr[j];
+                }
+            }
+            ans=max(ans,i-j);
+        }
+        cout<<ans<<endl;
 }
