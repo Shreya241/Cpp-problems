2e63fad560b9f2b27ecc173abf416c517d7c9f1a
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-02-18 01:52:03 +0530

    cp

--- CF solutions/Young phycist.cpp
+++ CF solutions/Young phycist.cpp
@@ -1,23 +1,32 @@
 #include<iostream>
 using namespace std;
-int main(){
-  int n;cin>>n;
-  int arr[n][3];
-  int s1=0,s2=0,s3=0;
-  for(int i=0;i<n;i++){
-    cin>>arr[i][0];
-    cin>>arr[i][1];
-    cin>>arr[i][2];
-  }
-  for(int i=0;i<n;i++){
-    s1+=arr[i][0];
-    s2+=arr[i][1];
-    s3+=arr[i][2];
-  }
-  
-  if(s1==0 && s2==0 && s3==0){
-    cout<<"YES";
-  }else{
-    cout<<"NO";
-  }
+
+int main()
+{
+    int t;
+    cin>>t;
+    int sum1=0,sum2=0,sum3=0;
+    int arr[t][3];
+    for(int i=0; i<t; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                cin>>arr[i][j];
+            }
+        }
+
+        for(int i=0; i<t; i++)
+        {
+            sum1 +=  arr[i][0];
+            sum2 +=  arr[i][1];
+            sum3 +=  arr[i][2];
+        }
+    if(sum1 ==0 && sum2==0 && sum3==0)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
 }
