3178f582a576b67f5c35d80eb1db17a1dbf5bc3d
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-05-13 14:06:50 +0530

    .

--- Greedy/Job sequencing.cpp
+++ Greedy/Job sequencing.cpp
@@ -1,10 +1,6 @@
-//{ Driver Code Starts
-// Program to find the maximum profit job sequence from a given array 
-// of jobs with deadlines and profits 
 #include<bits/stdc++.h>
 using namespace std; 
 
-// A structure to represent a job 
 struct Job 
 { 
     int id;	 // Job Id 
@@ -12,16 +8,6 @@ struct Job
     int profit; // Profit if job is over before or on deadline 
 }; 
 
-
-// } Driver Code Ends
-/*
-struct Job 
-{ 
-    int id;	 // Job Id 
-    int dead; // Deadline of job 
-    int profit; // Profit if job is over before or on deadline 
-};
-*/
 
 class Solution 
 {
@@ -57,8 +43,6 @@ class Solution
     } 
 };
 
-//{ Driver Code Starts.
-// Driver program to test methods 
 int main() 
 { 
     int t;
