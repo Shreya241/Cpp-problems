38a2bdc4b7e2474874f7e83bca630fa3397c8cbc
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-05-21 10:56:19 +0530

    queue problems

---
+++ Queue/circular tour.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+// } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       int sum=0;
+       int ind=0;
+       int bal=0;
+       for(int i=0;i<n;i++){
+           bal+=p[i].petrol-p[i].distance;
+           if(bal<0){
+               ind=i+1;;
+               sum+=bal;
+               bal=0;
+           }
+       }
+       if(sum+bal>=0){
+           return ind;
+       }else{
+           return -1;
+       }
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+
+// } Driver Code Ends
