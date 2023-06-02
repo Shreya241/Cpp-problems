58e184b70c9a6068a4e98ef0f58181973ed8fbd4
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-06-02 17:21:46 +0530

    stairs

---
+++ DP/count ways to reach the nth stairs.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair.
+    int countWays(int n)
+    {
+        vector<long long>v(n+1);
+        v[0]=1;v[1]=1;
+        for(int i=2;i<=n;i++){
+        v[i]=(v[i-1]+v[i-2])%1000000007;
+    }
+    return v[n];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}
+
+// } Driver Code Ends
