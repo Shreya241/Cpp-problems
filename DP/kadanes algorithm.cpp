385e4834c8aebabc8cc7dbb8a353734e836151d5
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-06-02 14:44:53 +0530

    kadane

---
+++ DP/kadanes algorithm.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+       vector<long long>v(n);
+       v[0]=arr[0];
+       long long res=arr[0];
+       for(int i=1;i<n;i++){
+           v[i]=max(v[i-1]+arr[i],(long long)arr[i]);
+           res=max(res,v[i]);
+       }
+       return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
