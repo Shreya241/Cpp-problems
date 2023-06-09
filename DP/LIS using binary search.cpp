5e0bd8e0d6eae26e0f2a2d4c233b4e23f3b9aa82
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-06-09 12:25:43 +0530

    bs lis

---
+++ DP/LIS using binary search.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    vector<int>v;
+    v.push_back(arr[0]);
+    for(int i=1;i<n;i++){
+        if(arr[i]>v.back()){
+            v.push_back(arr[i]);
+        }else{
+            int ind=lower_bound(v.begin(),v.end(),arr[i])-v.begin();
+            v[ind]=arr[i];
+        }
+    }
+    return v.size();
+}
