bf9320bce6963c08388467049653a9cdbb593b2f
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-06-14 23:04:25 +0530

    codechef

---
+++ Codechef contests/Gymkhana Election IIIT-A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>  
+#define int long long
+using namespace std;
+ 
+ 
+void funct(){
+  int n,m;
+  cin>>n>>m;
+  cout<<((m/2)+1)<<endl;
+}
+
+ 
+ 
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) funct();
+    return 0;
+}
