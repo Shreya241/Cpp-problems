c99e169729dc5a6b3589684a08b62fa8200bc398
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-06-15 13:04:17 +0530

    array

--- Striver 75/Array/best time to buy and sell stock.cpp
+++ Striver 75/Array/best time to buy and sell stock.cpp
@@ -15,3 +15,17 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int buyAtPrice = INT_MAX;
+        int maxProfit = 0;
+        for(int i=0; i<n; i++){
+            buyAtPrice = min(buyAtPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - buyAtPrice); 
+        }
+        return maxProfit;
+    }
+};
