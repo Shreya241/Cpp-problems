06f45c3c0fe81f60a5dab3347d013877e6a16a3b
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-07-02 08:53:14 +0530

    arrays

---
+++ arrays/is subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0,i=0;
+        while(i<s.size() && j< t.size()){
+            if(s[i]==t[j]){
+                i++,j++;
+            }else{
+                j++;
+            }
+        }
+        if(j==t.size() && i<s.size()){
+            return false;
+        }
+        return true;
+    }
+};
