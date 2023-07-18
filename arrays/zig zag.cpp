d687946f43b708005a773662a48230dd88aa24dc
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-07-18 11:54:48 +0530

    array

--- arrays/zig zag.cpp
+++ arrays/zig zag.cpp
@@ -2,24 +2,31 @@
 using namespace std;
 int main(){
     int n=7;
-    vector<int>v;
+    vector<int>v(n);
     for(int i=0;i<n;i++){
         cin>>v[i];
     }
+    reverse(v.begin(),v.end());
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<"->";
+    }
+    cout<<"NULL";
+    cout<<endl;
     bool flag=true;
     int p=0;
     while(p<n){
         if(flag){
-            if(v[p]>v[p+1]){
+            if(v[p]>v[p+1] && p!=n-1){
                 swap(v[p],v[p+1]);
             }
         }else{
-            if(v[p]<v[p+1]){
+            if(v[p]<v[p+1] && p!=n-1){
                 swap(v[p],v[p+1]);
             }
         }
         p++;
-        flag!=flag;
+        if(flag==true) flag=false;
+        else flag=true;
     }
     for(int i=0;i<n;i++){
         cout<<v[i]<<"->";
