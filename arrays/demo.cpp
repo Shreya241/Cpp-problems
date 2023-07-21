21d4122bb9a0872fea28929670e6a4a4867d5aec
Shreya241 <shreyaagrawal.kunra@gmail.com>
2023-07-21 08:00:21 +0530

    demo

--- arrays/demo.cpp
+++ arrays/demo.cpp
@@ -1,6 +1,26 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
+bool isPrime(int n){
+    if(n<=1){
+        return false;
+    }else{
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return false;
+        }
+    }
+    return true;
+}
 int main(){
-    cout<<(4|3);
+    int n;cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int a,b;
+    cin>>a>>b;
+    int ans=0;
+    for(int i=a;i<=b;i++){
+        if(isPrime(v[i])) ans++;
+    }
+    cout<<ans;
 }
